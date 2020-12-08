45735345ddefcfee2a4529c04179dacd301fe6f4
Shudipto Trafder <shudiptotrafder@gmail.com>
2020-12-08 22:29:52 +0600

    Added dark theme

---
+++ lib/utils/theme/themes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Themes {
+  static final light = ThemeData.light().copyWith(
+    backgroundColor: Colors.white,
+    buttonColor: Colors.blue,
+  );
+  static final dark = ThemeData.dark().copyWith(
+    backgroundColor: Colors.black,
+    buttonColor: Colors.red,
+  );
+}
