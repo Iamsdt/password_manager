8831a07e20fdbafef94cdd31b2fdd120ae1373dd
Shudipto Trafder <shudiptotrafder@gmail.com>
2020-12-09 09:31:05 +0600

    Password analysis

--- test/pass_ana.dart
+++ test/pass_ana.dart
@@ -2,5 +2,7 @@ import 'package:password_manager/utils/utils.dart';
 
 void main() {
   var pass = "Az0@";
-  print(Utils.analysisPassword(pass));
+  int res = Utils.analysisPassword(pass);
+
+  assert(res == 80);
 }
